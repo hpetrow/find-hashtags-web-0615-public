b768b811c0e224dbbbd3789c4c8125d086a1f89e
hpetrow <heather.petrow@gmail.com>
2015-06-08 20:25:10 -0400

    done

--- find_hashtag.rb
+++ find_hashtag.rb
@@ -1,3 +1,10 @@
 def get_hashtags(tweet)
   #code here
+  words = tweet.split(" ").collect { |word|
+    word.scan(/\A#+[a-zA-Z]+\z/)
+  }.flatten
+
+  words.collect { |word|
+    word.gsub("#", "")
+  }
 end
